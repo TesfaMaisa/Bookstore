226d4d92b7670f5499f8aeb97da8370091af0e89
TesfaMaisa <bhm236@myy.haaga-helia.fi>
2025-09-06 14:31:15 +0300

    Päivitetty

--- src/main/java/bookstore/bookstore/web/BookController.java
+++ src/main/java/bookstore/bookstore/web/BookController.java
@@ -4,8 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import bookstore.bookstore.domain.Book;
 import bookstore.bookstore.domain.BookRepository;
 
 
@@ -17,9 +20,16 @@ public class BookController {
         this.repository = repository;
     }
 
-    @GetMapping("/books")
-    public String showBooks(Model model){
-        model.addAttribute("books", repository.findAll());
+    @GetMapping("books")
+    public String addBook(Model model){
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("books")
+    public String showBooks(@ModelAttribute Book book, Model model){
+        model.addAttribute("repo", repository.findAll());
+        model.addAttribute("books", book);
         return "booklist";
     }
 
